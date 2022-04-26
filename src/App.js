a0444c60f023b7cfc1c26a3b51b681eae041093a
Jakobi Haskell <jakobi_haskell@brown.edu>
2022-04-26 04:46:18 -0400

    working version

--- src/App.js
+++ src/App.js
@@ -32,69 +32,94 @@ function App() {
       }}
       graph={[
         {
-          type: "BarGraph",
+          type: "RectangleChart",
           data: {
-            dataFile: "data.csv",
+            dataFile: "flightTrends.csv",
             fileType: "csv",
             fieldDesc: [
-              ["x", "text"],
-              ["z", "text"],
-              ["height", "number"],
+              ["Year", "number"],
+              ["Domestic", "number"],
+              ["International", "number"],
+              ["Total", "number"],
             ],
           },
           style: {
+            origin: { x: 0, y: 0, z: 0 },
             dimensions: {
-              width: 20,
-              height: 5,
+              width: 10,
+              height: 10,
               depth: 10,
             },
           },
           mark: {
-            type: "box",
             position: {
               x: {
-                field: "x",
-              },
-              z: {
-                field: "z",
+                field: "Year",
               },
             },
+            type: "box",
             style: {
+              padding: {
+                x: 0.1,
+              },
+              depth: {
+                field: "Domestic",
+              },
               height: {
-                startFromZero: true,
-                field: "height",
+                field: "Total",
               },
               fill: {
-                opacity: 0.8,
+                opacity: 0.4,
                 scaleType: "linear",
-                field: "height",
-                color: ["#b71c1c", "#2196f3"],
+                field: "International",
+                color: ["red", "green"],
+              },
+            },
+            mouseOver: {
+              focusedObject: {
+                opacity: 1,
+                fill: "#333",
+              },
+              nonFocusedObject: {
+                opacity: 0,
+              },
+              label: {
+                value: (d) =>
+                  `Year:${d.Year}\nDomestic Flights:${d.Domestic}\nInternational Flights:${d.International}\nTotal Flights:${d.Total}\n`,
+                align: "center",
+                fontSize: 1,
+                backgroundColor: "#333",
+                backgroundOpacity: 1,
+                fontColor: "#fff",
               },
             },
           },
           axis: {
+            "axis-box": {
+              color: "gray",
+            },
             "x-axis": {
-              orient: "back-bottom",
+              orient: "bottom-back",
               ticks: {
                 noOfTicks: 10,
-                size: 0.01,
+                size: 1,
                 color: "white",
                 opacity: 0.7,
-                fontSize: 3,
+                fontSize: 10,
               },
               grid: {
                 color: "white",
-                opacity: 0.7,
+                opacity: 0.8,
               },
             },
             "y-axis": {
-              orient: "back-left",
+              orient: "bottom-back",
               ticks: {
-                noOfTicks: 10,
-                size: 0.01,
+                noOfTicks: 5,
+                size: 4,
                 color: "white",
                 opacity: 0.7,
-                fontSize: 3,
+                fontSize: 10,
               },
               grid: {
                 color: "white",
@@ -102,21 +127,246 @@ function App() {
               },
             },
             "z-axis": {
-              orient: "bottom-left",
+              orient: "bottom-back",
+              ticks: {
+                noOfTicks: 5,
+                size: 1,
+                color: "white",
+                opacity: 0.5,
+                fontSize: 12,
+              },
+              grid: {
+                color: "white",
+                opacity: 0.8,
+              },
+            },
+          },
+        },
+
+        {
+          type: "RectangleChart",
+          data: {
+            dataFile: "2020flightTrends.csv",
+            fileType: "csv",
+            fieldDesc: [
+              ["Month", "number"],
+              ["Domestic", "number"],
+              ["International", "number"],
+              ["Total", "number"],
+            ],
+          },
+          style: {
+            origin: { x: 0, y: 0, z: -40 },
+            dimensions: {
+              width: 10,
+              height: 10,
+              depth: 10,
+            },
+          },
+          mark: {
+            position: {
+              x: {
+                field: "Month",
+              },
+            },
+            type: "box",
+            style: {
+              padding: {
+                x: 0.1,
+              },
+              depth: {
+                field: "Domestic",
+              },
+              height: {
+                field: "Total",
+              },
+              fill: {
+                opacity: 0.4,
+                scaleType: "linear",
+                field: "International",
+                color: ["red", "green"],
+              },
+            },
+            mouseOver: {
+              focusedObject: {
+                opacity: 1,
+                fill: "#333",
+              },
+              nonFocusedObject: {
+                opacity: 0,
+              },
+              label: {
+                value: (d) =>
+                  `Months (from 2020):${d.Month}\n Domestic Flights (Monthly):${d.Domestic}\nInternational Flights (Monthly):${d.International}\nTotal Flights (Monthly):${d.Total}\n`,
+                align: "center",
+                fontSize: 1,
+                backgroundColor: "#333",
+                backgroundOpacity: 1,
+                fontColor: "#fff",
+              },
+            },
+          },
+          axis: {
+            "axis-box": {
+              color: "gray",
+            },
+            "x-axis": {
+              orient: "bottom-back",
               ticks: {
                 noOfTicks: 10,
-                size: 0.01,
+                size: 1,
                 color: "white",
                 opacity: 0.7,
-                fontSize: 3,
+                fontSize: 10,
+              },
+              grid: {
+                color: "white",
+                opacity: 0.8,
+              },
+            },
+            "y-axis": {
+              orient: "bottom-back",
+              ticks: {
+                noOfTicks: 5,
+                size: 4,
+                color: "white",
+                opacity: 0.7,
+                fontSize: 10,
               },
               grid: {
                 color: "white",
                 opacity: 0.7,
               },
             },
+            "z-axis": {
+              orient: "bottom-back",
+              ticks: {
+                noOfTicks: 5,
+                size: 1,
+                color: "white",
+                opacity: 0.5,
+                fontSize: 12,
+              },
+              grid: {
+                color: "white",
+                opacity: 0.8,
+              },
+            },
           },
         },
+
+        {
+          type: "RectangleChart",
+          data: {
+            dataFile: "monthly_international_and_us_cases.csv",
+            fileType: "csv",
+            fieldDesc: [
+              ["month", "number"],
+              ["year", "number"],
+              ["newPositiveCasesUS", "number"],
+              ["newPositiveCasesInternational", "number"],
+            ],
+          },
+          style: {
+            origin: { x: 0, y: 0, z: -80 },
+            dimensions: {
+              width: 10,
+              height: 10,
+              depth: 10,
+            },
+          },
+          mark: {
+            position: {
+              x: {
+                field: "month",
+              },
+            },
+            type: "box",
+            style: {
+              padding: {
+                x: 0.1,
+              },
+              depth: {
+                field: "newPositiveCasesInternational",
+              },
+              height: {
+                field: "newPositiveCasesUS",
+              },
+              fill: {
+                opacity: 0.4,
+                scaleType: "linear",
+                field: "newPositiveCasesUS",
+                color: ["red", "green"],
+              },
+            },
+            mouseOver: {
+              focusedObject: {
+                opacity: 1,
+                fill: "#333",
+              },
+              nonFocusedObject: {
+                opacity: 0,
+              },
+              label: {
+                value: (d) =>
+                  `Months (from 2020):${d.month}\n US New Cases (Monthly):${d.newPositiveCasesUS}\nInternational New Cases (Monthly):${d.newPositiveCasesInternational}\n`,
+                align: "center",
+                fontSize: 1,
+                backgroundColor: "#333",
+                backgroundOpacity: 1,
+                fontColor: "#fff",
+              },
+            },
+          },
+          axis: {
+            "axis-box": {
+              color: "gray",
+            },
+            "x-axis": {
+              orient: "bottom-back",
+              ticks: {
+                noOfTicks: 10,
+                size: 1,
+                color: "white",
+                opacity: 0.7,
+                fontSize: 10,
+              },
+              grid: {
+                color: "white",
+                opacity: 0.8,
+              },
+            },
+            "y-axis": {
+              orient: "bottom-back",
+              ticks: {
+                noOfTicks: 5,
+                size: 4,
+                color: "white",
+                opacity: 0.7,
+                fontSize: 10,
+              },
+              grid: {
+                color: "white",
+                opacity: 0.7,
+              },
+            },
+            "z-axis": {
+              orient: "bottom-back",
+              ticks: {
+                noOfTicks: 5,
+                size: 1,
+                color: "white",
+                opacity: 0.5,
+                fontSize: 12,
+              },
+              grid: {
+                color: "white",
+                opacity: 0.8,
+              },
+            },
+          },
+        },
+        
       ]}
     />
   );
---
+++ src/SecondVRViz.js
@@ -0,0 +1,122 @@
+import React from "react";
+import VRViz from "vr-viz";
+
+export default function SecondVRViz() {
+  return (
+    <VRViz
+      graph={[
+        {
+          type: "RectangleChart",
+          data: {
+            dataFile: "monthly_international_and_us_cases.csv",
+            fileType: "csv",
+            fieldDesc: [
+              ["month", "number"],
+              ["year", "number"],
+              ["newPositiveCasesUS", "number"],
+              ["newPositiveCasesInternational", "number"],
+            ],
+          },
+          style: {
+            origin: { x: 0, y: 0, z: -80 },
+            dimensions: {
+              width: 10,
+              height: 10,
+              depth: 10,
+            },
+          },
+          mark: {
+            position: {
+              x: {
+                field: "month",
+              },
+            },
+            type: "box",
+            style: {
+              padding: {
+                x: 0.1,
+              },
+              depth: {
+                field: "newPositiveCasesInternational",
+              },
+              height: {
+                field: "newPositiveCasesUS",
+              },
+              fill: {
+                opacity: 0.4,
+                scaleType: "linear",
+                field: "newPositiveCasesUS",
+                color: ["red", "green"],
+              },
+            },
+            mouseOver: {
+              focusedObject: {
+                opacity: 1,
+                fill: "#333",
+              },
+              nonFocusedObject: {
+                opacity: 0,
+              },
+              label: {
+                value: (d) =>
+                  `Months (from 2020):${d.month}\n US New Cases (Monthly):${d.newPositiveCasesUS}\nInternational New Cases (Monthly):${d.newPositiveCasesInternational}\n`,
+                align: "center",
+                fontSize: 1,
+                backgroundColor: "#333",
+                backgroundOpacity: 1,
+                fontColor: "#fff",
+              },
+            },
+          },
+          axis: {
+            "axis-box": {
+              color: "gray",
+            },
+            "x-axis": {
+              orient: "bottom-back",
+              ticks: {
+                noOfTicks: 10,
+                size: 1,
+                color: "white",
+                opacity: 0.7,
+                fontSize: 10,
+              },
+              grid: {
+                color: "white",
+                opacity: 0.8,
+              },
+            },
+            "y-axis": {
+              orient: "bottom-back",
+              ticks: {
+                noOfTicks: 5,
+                size: 4,
+                color: "white",
+                opacity: 0.7,
+                fontSize: 10,
+              },
+              grid: {
+                color: "white",
+                opacity: 0.7,
+              },
+            },
+            "z-axis": {
+              orient: "bottom-back",
+              ticks: {
+                noOfTicks: 5,
+                size: 1,
+                color: "white",
+                opacity: 0.5,
+                fontSize: 12,
+              },
+              grid: {
+                color: "white",
+                opacity: 0.8,
+              },
+            },
+          },
+        },
+      ]}
+    ></VRViz>
+  );
+}
